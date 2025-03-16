955a9eec741d4761b2872e1a54e70c9c86094338
Nikita <bletigri@gmail.com>
2025-03-16 15:00:23 +0300

    end work now

--- test.py
+++ test.py
@@ -4,13 +4,25 @@ from configparser import ConfigParser
 import requests
 
 
-
-def rayon(id_reg):
-    ...
+def districts(region_id, API):
+    req = requests.get(f'http://geohelper.info/api/v1/districts?pagination[limit]=100&regionId={region_id}&locale[lang]=ru&apiKey={API}')
+    rayons = req.json()
+    print(rayons)
 
 
 def region(iso, API):
-    req = requests.get(f'http://geohelper.info/api/v1/regions?filter[countryIso]={iso}&locale[lang]=ru&apiKey={API}')
+    req = requests.get(f'http://geohelper.info/api/v1/regions?pagination[limit]=100&filter[countryIso]={iso}&locale[lang]=ru&apiKey={API}')
+    regions = req.json()
+    pagination = regions['pagination']
+
+    for page in range(pagination['totalPageCount']):
+        req = requests.get(f'http://geohelper.info/api/v1/regions?pagination[page]={page}&pagination[limit]=100&filter[countryIso]={iso}&locale[lang]=ru&apiKey={API}')
+        regions = req.json()
+        
+        for i in regions['result']:
+            print(f'    ---> ID: {i['id']} Имя региона: {i['name']}')
+            # districts(i['id'], API)
+            break
 
 
 def main():
@@ -21,9 +33,10 @@ def main():
 
     req = requests.get(f'http://geohelper.info/api/v1/countries?locale[lang]=ru&apiKey={API}')  
     countries = req.json()
-
+    
     for i in countries['result']:
-        print(i)
+        # print(f'ISO: {i['iso']} Имя страны: {i['name']}')
+        region(i['iso'], API)
         break
 
 
